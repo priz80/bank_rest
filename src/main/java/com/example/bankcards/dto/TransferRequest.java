c796165571925442702a742f969ed479732274dc
IGOR <127472053+priz80@users.noreply.github.com>
2026-03-10 02:51:27 +0300

    Update TransferRequest.java

--- src/main/java/com/example/bankcards/dto/TransferRequest.java
+++ src/main/java/com/example/bankcards/dto/TransferRequest.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TransferRequest {
     @NotNull(message = "Source card ID is required")
     private Long fromCardId;
