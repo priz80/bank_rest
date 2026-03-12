4cb471ecbf137c90c03bcd39de195c64fb109cd7
IGOR <127472053+priz80@users.noreply.github.com>
2026-03-12 12:46:31 +0300

    Update CreateUserRequest.java

--- src/main/java/com/example/bankcards/dto/CreateUserRequest.java
+++ src/main/java/com/example/bankcards/dto/CreateUserRequest.java
@@ -1,10 +1,15 @@
 package com.example.bankcards.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserRequest {
+     @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
-    private Long userId;
 }
