02baec75a302c6d33f7c87b455d18449b8ca569a
IGOR <127472053+priz80@users.noreply.github.com>
2026-03-12 23:56:24 +0300

    Update OpenApi3Config.java

--- src/main/java/com/example/bankcards/config/OpenApi3Config.java
+++ src/main/java/com/example/bankcards/config/OpenApi3Config.java
@@ -2,7 +2,6 @@ package com.example.bankcards.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +12,7 @@ public class OpenApi3Config {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
+        String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
@@ -22,10 +21,6 @@ public class OpenApi3Config {
                                         .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .info(new Info()
-                        .title("Bank Cards API")
-                        .version("1.0")
-                        .description("API for managing bank cards and transfers"));
+                                        .bearerFormat("JWT")));
     }
 }
