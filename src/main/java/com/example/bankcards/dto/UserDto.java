e9cad6b089f26cd9b99f46b265896edd38120196
IGOR <127472053+priz80@users.noreply.github.com>
2026-03-12 23:50:36 +0300

    Update UserDto.java

--- src/main/java/com/example/bankcards/dto/UserDto.java
+++ src/main/java/com/example/bankcards/dto/UserDto.java
@@ -2,7 +2,6 @@ package com.example.bankcards.dto;
 
 import com.example.bankcards.entity.Role;
 import com.example.bankcards.entity.User.Status;
-
 import lombok.Data;
 
 @Data
@@ -10,5 +9,5 @@ public class UserDto {
     private Long id;
     private String username;
     private Role role;
-     private Status status;
+    private Status status;
 }
