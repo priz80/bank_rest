f9157b259923448653fd74e04ee46b687c1d579a
IGOR <127472053+priz80@users.noreply.github.com>
2026-03-12 23:54:33 +0300

    Update CardDto.java

--- src/main/java/com/example/bankcards/dto/CardDto.java
+++ src/main/java/com/example/bankcards/dto/CardDto.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.math.BigDecimal;
 import java.time.YearMonth;
 
-public class CardDto {
+import lombok.Data;
 
+@Data
+public class CardDto {
     private Long id;
     private String maskedCardNumber;
     private String cardHolderName;
@@ -17,61 +19,4 @@ public class CardDto {
     private CardStatus status;
     private BigDecimal balance;
     private Long userId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMaskedCardNumber() {
-        return maskedCardNumber;
-    }
-
-    public void setMaskedCardNumber(String maskedCardNumber) {
-        this.maskedCardNumber = maskedCardNumber;
-    }
-
-    public String getCardHolderName() {
-        return cardHolderName;
-    }
-
-    public void setCardHolderName(String cardHolderName) {
-        this.cardHolderName = cardHolderName;
-    }
-
-    @JsonFormat(pattern = "yyyy-MM")
-    public YearMonth getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(YearMonth expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public CardStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(CardStatus status) {
-        this.status = status;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
 }
